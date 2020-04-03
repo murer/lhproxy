579f5a02002b97fdcf4b209eaf7812d22c14ca6e
murer <murer@dextra-sw.com>
2020-04-02 21:33:06 -0300

    x

--- util/util_test.go
+++ util/util_test.go
@@ -7,17 +7,7 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
-func TestCheckSuccess(t *testing.T) {
+func TestCheck(t *testing.T) {
   Check(nil)
   assert.Panics(t, func() { Check(errors.New("mock error")) })
-}
-
-func TestCheckError(t *testing.T) {
-  defer func() {
-    err := recover()
-    if err == nil {
-      t.Errorf("Panic was required")
-    }
-  }()
-  Check(errors.New("mock error"))
 }
