57c2f44f096c2f71cbc076a74f005d062e17f787
murer <murer@dextra-sw.com>
2020-04-03 14:40:37 -0300

    x

--- util/queue/queue_test.go
+++ util/queue/queue_test.go
@@ -13,6 +13,22 @@ func TestQueueInt(t *testing.T) {
 	assert.Equal(t, 2, len(q.l))
 	assert.Equal(t, 10, q.Shift())
 	assert.Equal(t, 20, q.Shift())
+	assert.Equal(t, 0, len(q.l))	
+}
+
+func TestQueueStruct(t *testing.T) {
+	type mystruct struct {
+		N string
+	}
+	q := New(2)
+	q.Put(&mystruct{"a"})
+	q.Put(&mystruct{"b"})
+	assert.Equal(t, 2, len(q.l))
+	assert.Equal(t, &mystruct{"a"}, q.Shift())
+	assert.Equal(t, &mystruct{"b"}, q.Shift())
+	assert.Equal(t, 0, len(q.l))
+}
+
 
 
 	// go func() {
@@ -31,4 +47,3 @@ func TestQueueInt(t *testing.T) {
 	// time.Sleep(50 * time.Millisecond)
 	// assert.Equal(t, 3, g.Shift())
 	// assert.Nil(t, g.Shift())
-}
