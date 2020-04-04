4a59483d9572c9b11c472daa04f62c35fa153f25
murer <murer@dextra-sw.com>
2020-04-04 15:36:34 -0300

    x

--- build.sh
+++ build.sh
@@ -11,12 +11,10 @@ cmd_golang() {
 }
 
 cmd_test() {
-  # --proxy http://C80795A:QWEasd1@@localhost:6001
   cmd_golang go test ./client ./server ./util ./util/queue ./test "$@"
 }
 
 cmd_curl_test() {
-  #--proxy-pass 'QWEasd1@' --proxy-user C80795A --proxy-basic --proxy http://localhost:6001/ \
   curl -v \
      --no-buffer \
      --header "Connection: Upgrade" \
@@ -28,13 +26,3 @@ cmd_curl_test() {
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
-
-# GET http://localhost:6001/ HTTP/1.1
-# Host: t1.test.serasa.a.vpn.dextra.com.br
-# User-Agent: Go-http-client/1.1
-# Connection: Upgrade
-# Proxy-Authorization: Basic QzgwNzk1QTpRV0Vhc2QxQA==
-# Sec-WebSocket-Key: FYtltTOZX/HBH2iT1BQxgQ==
-# Sec-WebSocket-Protocol: chisel-v3
-# Sec-WebSocket-Version: 13
-# Upgrade: websocket
