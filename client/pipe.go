b8a60780a302e46bab0ee393b5d9c3357e0e34ee
murer <murer@dextra-sw.com>
2020-04-04 14:02:15 -0300

    x

--- client/pipe.go
+++ client/pipe.go
@@ -16,21 +16,11 @@ type Pipe struct {
 
 func (c Pipe) Execute() {
 	defer c.Writer.Close()
+
+	scks.Connect(c.Address)
+
 	log.Printf("Start pipe to %s", c.Address)
 	_, err := io.Copy(c.Writer, c.Reader)
 	util.Check(err)
 	log.Printf("Pipe to %s is done", c.Address)
-	// buf := make([]byte, 8 * 1024)
-	// for true {
-	// 	n, err := c.LReader.Read(buf)
-	// 	if n > 0 {
-	// 		c.LWriter.Write([]byte("LINE: "))
-	// 		c.LWriter.Write(buf[:n])
-	// 		c.LWriter.Write([]byte{10})
-	// 	}
-	// 	if err == io.EOF {
-	// 		return
-	// 	}
-	// 	util.Check(err)
-	// }
 }
