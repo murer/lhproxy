a7b6a5955b19bcd1ba77f0279cc92fcfca7ba6a2
murer <murer@dextra-sw.com>
2020-04-04 13:40:35 -0300

    x

--- client/pipe.go
+++ client/pipe.go
@@ -1,31 +1,29 @@
 package client
 
 import (
-  "io"
-	"github.com/murer/lhproxy/util"
+	"io"
+	"github.com/murer/lhproxy/sockets"
 )
 
 type Pipe struct {
-
-  RAddress string
-  LHAddress string
-
-  LWriter io.Writer
-  LReader io.Reader
+	Scks sockets.Sockets
+	Address string
+	Writer io.Writer
+	Reader io.Reader
 }
 
 func (c Pipe) Execute() {
-  buf := make([]byte, 8 * 1024)
-  for true {
-    n, err := c.LReader.Read(buf)
-    if n > 0 {
-      c.LWriter.Write([]byte("LINE: "))
-      c.LWriter.Write(buf[:n])
-      c.LWriter.Write([]byte{10})
-    }
-    if err == io.EOF {
-      return
-    }
-    util.Check(err)
-  }
+	// buf := make([]byte, 8 * 1024)
+	// for true {
+	// 	n, err := c.LReader.Read(buf)
+	// 	if n > 0 {
+	// 		c.LWriter.Write([]byte("LINE: "))
+	// 		c.LWriter.Write(buf[:n])
+	// 		c.LWriter.Write([]byte{10})
+	// 	}
+	// 	if err == io.EOF {
+	// 		return
+	// 	}
+	// 	util.Check(err)
+	// }
 }
---
+++ client/pipe_test.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"testing"
+	"time"
+	"io"
+	// "github.com/stretchr/testify/assert"
+	"github.com/murer/lhproxy/sockets"
+)
+
+func TestPipe(t *testing.T) {
+	scks := &sockets.NativeSockets{
+		ReadTimeout: 1 * time.Millisecond,
+		AcceptTimeout: 1 * time.Millisecond,
+	}
+	lr, lw := io.Pipe()
+	defer lw.Close()
+	defer lr.Close()
+	rr, rw := io.Pipe()
+	defer rw.Close()
+	defer rr.Close()
+
+	sckid := scks.Listen("localhost:5001")
+	defer scks.Close(sckid, sockets.CLOSE_SCK)
+	p := &Pipe{
+		Scks: scks,
+		Address: "localhost:5001",
+		Reader: lr,
+		Writer: rw,
+	}
+	p.Execute()
+}
