14a9ae14e6330f076e0bcc5550fd4efdc443f877
murer <murer@dextra-sw.com>
2020-04-05 22:49:14 -0300

    x

--- it/it.sh
+++ it/it.sh
@@ -1,23 +1,31 @@
 #!/bin/bash -xe
 
-cd "$(dirname "$0")"
-
-cleanup() {
+cmd_cleanup() {
   docker rm -f lhproxy_it_squid || true
   docker rm -f lhproxy_it_pipe || true
 }
-trap cleanup EXIT
 
-cleanup
+cmd_build() {
+  cd ..
+  docker build -t lhproxy/it:dev -f it/Dockerfile .
+  cd -
+}
 
-cd ..
-docker build -t lhproxy/it:dev -f it/Dockerfile .
-cd -
+cmd_test() {
+  trap cmd_cleanup EXIT
+  cmd_cleanup
+  docker run -d --rm --label lhproxy_dev --name lhproxy_it_squid \
+    -p 3128:3128 -h lhproxy_it_squid lhproxy/it:dev /root/entrypoint/server.sh
 
-docker run -d --rm --label lhproxy_dev --name lhproxy_it_squid \
-  -p 3128:3128 -h lhproxy_it_squid lhproxy/it:dev /root/entrypoint/server.sh
+  docker run -it --rm --label lhproxy_dev --name lhproxy_it_pipe \
+    --network host lhproxy/it:dev /root/entrypoint/pipe.sh
 
-docker run -it --rm --label lhproxy_dev --name lhproxy_it_pipe \
-  --network host lhproxy/it:dev /root/entrypoint/pipe.sh
+  echo "SUCCESS"
+}
 
-echo "SUCCESS"
+cmd_it() {
+  cmd_build
+  cmd_test
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
