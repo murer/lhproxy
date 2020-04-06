41f601d576e9f8dd8e47e4b9708ac1bc0bbbbaa8
murer <murer@dextra-sw.com>
2020-04-06 13:39:54 -0300

    x

--- ci/ci.sh
+++ ci/ci.sh
@@ -14,7 +14,7 @@ cmd_build() {
   # ./it/it.sh build base 1> /dev/null 2>&1 &
   ./docker.sh runi golang ./build.sh test .
   ./docker.sh runi golang ./build.sh build_all "$LHPROXY_VERSION"
-  wait %1
+  # wait %1
   # ./it/it.sh it
   ./docker.sh build
   [[ -z "$(git status --porcelain)" ]]
