752621034f0e01d9c2b6f5940eba17de9ed868a7
murer <murer@dextra-sw.com>
2020-04-07 17:06:10 -0300

    x

--- it/http/build.sh
+++ it/http/build.sh
@@ -9,9 +9,9 @@ cmd_cleanup() {
 cmd_run() {
   cmd_cleanup
   docker network create --driver bridge lhproxy-it-http --label lhproxy_dev
-  docker run -dit --name a1 --network lhproxy-it-http alpine:3.11 sh
-  docker run -dit --name a2 --network lhproxy-it-http alpine:3.11 sh
-  docker exec -it a1 ping a2
+  docker run -dit --name a1 --network lhproxy-it-http --label lhproxy_dev alpine:3.11 sh
+  docker run -dit --name a2 --network lhproxy-it-http --label lhproxy_dev alpine:3.11 sh
+  docker exec -it a1 ping a2 -c 2
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
