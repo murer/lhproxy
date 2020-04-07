5411d51ef146dd65e4f76486197421eb430dd4c8
murer <murer@dextra-sw.com>
2020-04-07 17:55:10 -0300

    xy

--- it/http/build.sh
+++ it/http/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -xe
 
 cmd_cleanup() {
-  docker rm -f a1 a2 || true
-  docker network rm lhproxy-it-http || true
+  docker rm -f lhproxy-it-nginx lhproxy-it || true
+  docker network rm lhproxy-it-nginx || true
   true
 }
 
 cmd_run() {
   cmd_cleanup
-  docker network create --driver bridge lhproxy-it-http --label lhproxy_dev
-  docker run -dit --name a1 --network lhproxy-it-http --label lhproxy_dev alpine:3.11 sh
-  docker run -dit --name a2 --network lhproxy-it-http --label lhproxy_dev alpine:3.11 sh
-  docker exec -it a1 ping a2 -c 2
+  docker network create --driver bridge lhproxy-it-nginx --label lhproxy_dev
+  docker run -dit --rm --network lhproxy-it-nginx --name lhproxy-it-nginx nginx:1.17
+  docker run -it --rm --network lhproxy-it-nginx --name lhproxy-it curlimages/curl:7.69.1 curl --f http://lhproxy-it-nginx/
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
