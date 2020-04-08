d2fa6ae1621258b3ba1ddff0cad90d67609c3526
murer <murer@dextra-sw.com>
2020-04-08 16:01:57 -0300

    xy

--- server/server_test.go
+++ server/server_test.go
@@ -27,13 +27,14 @@ func TestSelf(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(Handle))
 	defer server.Close()
 	t.Logf("URL: %s", server.URL)
-	resp, err := http.Get(server.URL + "/self/lhproxy")
-	util.Check(err)
-	defer resp.Body.Close()
-	assert.Equal(t, 200, resp.StatusCode)
 	path, err := os.Executable()
 	util.Check(err)
 	localData, err := ioutil.ReadFile(path)
 	util.Check(err)
+
+	resp, err := http.Get(server.URL + "/self/lhproxy")
+	util.Check(err)
+	defer resp.Body.Close()
+	assert.Equal(t, 200, resp.StatusCode)
 	assert.Equal(t, util.SHA256(localData), util.SHA256(util.ReadAll(resp.Body)))
 }
