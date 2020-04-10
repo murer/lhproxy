6caf696d676afb36740f1059555efc1d6eaa8444
murer <murer@dextra-sw.com>
2020-04-10 12:38:33 -0300

    xy

--- server/tunnel.go
+++ server/tunnel.go
@@ -1,15 +1,47 @@
 package server
 
 import (
-
+	// "net/http"
+	"log"
+	// "github.com/murer/lhproxy/util"
 )
 
-type Tunnel struct {}
-
-func (t *Tunnel) Request(r *Message) *Message {
-	return r
+type Tunnel struct {
+	URL string
+	channel chan *Message
 }
 
-func (t *Tunnel) Post() {
+func NewTunnel(url string) *Tunnel {
+	return &Tunnel{
+		URL: url,
+		channel: make(chan *Message, 2),
+	}
+}
 
+func (me *Tunnel) Request(req *Message) *Message {
+	log.Printf("Sending: %s", req.Name)
+	me.channel <- req
+	log.Printf("Message sent: %s", req.Name)
+	resp := <- me.channel
+	log.Printf("Received: %s -> %s", req.Name, resp.Name)
+	return resp
+}
+
+func (me *Tunnel) Post() {
+	// reader := &Reader{}
+	// resp, err := http.Post(me.URL, "application/octet-stream", reader)
+	// util.Check(err)
+	// log.Printf("code: %d", resp.StatusCode)
+	log.Printf("redirecting messages")
+	// for true {
+		req := <- me.channel
+		log.Printf("redirect req: %s", req.Name)
+		me.channel <- req
+	// }
+}
+
+type Reader struct {}
+
+func (me *Reader) Read(buf []byte) (int, error) {
+	return 0, nil
 }
--- server/tunnel_test.go
+++ server/tunnel_test.go
@@ -1,18 +1,22 @@
 package server
 
 import (
-  "net/http"
+	"net/http"
 	"net/http/httptest"
 	"testing"
-
+	"time"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTunnel(t *testing.T) {
-  server := httptest.NewServer(http.HandlerFunc(Handle))
+	server := httptest.NewServer(http.HandlerFunc(Handle))
 	defer server.Close()
 	t.Logf("URL: %s", server.URL)
-  tunnel := &Tunnel{}
+	tunnel := NewTunnel(server.URL)
 	original := &Message{Name: "echo", Payload: []byte{10}}
-  assert.Equal(t, original, tunnel.Request(original))
+	go func() {
+		time.Sleep(1 * time.Millisecond)
+		tunnel.Post()
+	}()
+	assert.Equal(t, original, tunnel.Request(original))
 }
