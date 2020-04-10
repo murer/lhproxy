1597ce5d5b2144fbcde1ea5429a22d846b9a5d7c
murer <murer@dextra-sw.com>
2020-04-10 11:47:48 -0300

    xy

--- server/tunnel_test.go
+++ server/tunnel_test.go
@@ -1,11 +1,17 @@
 package server
 
 import (
-	"github.com/stretchr/testify/assert"
+  "net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTunnel(t *testing.T) {
+  server := httptest.NewServer(http.HandlerFunc(Handle))
+	defer server.Close()
+	t.Logf("URL: %s", server.URL)
   tunnel := &Tunnel{}
 	original := &Message{Name: "echo", Payload: []byte{10}}
   assert.Equal(t, original, tunnel.Request(original))
